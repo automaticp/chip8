d420d2d159f96c2d37eeea4c6dd46a339b134c4e
automaticp <automaticpte@gmail.com>
2022-12-02 20:09:40 +0300

    update code in main for use with Canvas

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,111 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Vector2.hpp>
-#include <SFML/Window/ContextSettings.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Window/Keyboard.hpp>
-#include <SFML/Window/VideoMode.hpp>
-#include <SFML/Graphics/Transform.hpp>
-#include <SFML/Window/WindowStyle.hpp>
-#include <cmath>
+#include "Canvas.hpp"
+#include "Chip8.hpp"
+#include <fmt/format.h>
+#include <chrono>
+#include <ios>
+#include <iterator>
+#include <thread>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <vector>
+#include <string>
+
+using frame = std::chrono::duration<double, std::ratio<1, 60>>;
+
+constexpr size_t cycles_per_frame{ 10 };
 
 
-int main() {
+std::optional<std::vector<Byte>> read_binary(const std::string& file) {
 
-    sf::ContextSettings settings{};
-    settings.majorVersion = 3;
-    settings.minorVersion = 3;
-    settings.antialiasingLevel = 8;
+    std::ifstream fs{ file, std::ios_base::binary };
 
-    sf::RenderWindow window{
-        sf::VideoMode{800, 600}, "Hello, SFML!", sf::Style::Default, settings
+    if (!fs.fail()) {
+        try {
+            return std::vector<Byte>{
+                std::istreambuf_iterator<char>(fs),
+                std::istreambuf_iterator<char>(),
+            };
+        } catch (std::ios_base::failure&) {
+            return {};
+        }
+    }
+    return {};
+}
+
+
+void print_fb(const Chip8::framebuffer_t& fb, Short opcode) {
+
+    auto to_hex_char = [](size_t i) {
+        return "0123456789ABCDEF"[i % 0x10];
     };
-    window.setVerticalSyncEnabled(true);
 
-    sf::CircleShape shape{ 100.f };
-    shape.setFillColor(sf::Color::Green);
+    fmt::print("\n{:#06x}\n", (opcode));
+
+    const auto& fb_width = Chip8Base::fb_width;
+    std::string line_buf(fb_width, ' ');
+    fmt::print("   0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF\n\n");
+    for (size_t line{ 0 }; line < Chip8Base::fb_height; ++line) {
+        for (size_t col{ 0 }; col < line_buf.size(); ++col) {
+            size_t pos{ line * fb_width + col };
+            line_buf[col] = fb[pos] ? 'X' : '.';
+        }
+        fmt::print("{:2} {}\n", to_hex_char(line), line_buf);
+    }
+}
 
 
-    sf::Clock clock{};
 
-    constexpr float speed{ 5.0f };
+int main(int argc, const char* argv[]) {
+
+    // Argument parser would be nice, maybe...
+    if (argc < 2) {
+        std::cout << "Usage:\n    chip8 [file]\n";
+        return 0;
+    }
+
+    std::string file{ argv[1] };
+    auto program = read_binary(file);
+    if (!program.has_value()) {
+        std::cerr << "Unable to open file: " << file << '\n';
+        return 1;
+    }
+
+
+    Canvas canvas{};
+    auto& window = canvas.window();
+
+    Chip8 chip8{};
+    chip8.load_program(program.value());
+
+    auto frame_clock =
+        std::chrono::time_point_cast<frame>(
+            std::chrono::steady_clock::now()
+        );
 
     while (window.isOpen()) {
+        frame_clock =
+            std::chrono::time_point_cast<frame>(std::chrono::steady_clock::now())
+            + frame{ 1 };
 
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            } else if (event.type == sf::Event::KeyPressed) {
-                switch (event.key.code) {
-                    case sf::Keyboard::Key::W:
-                        shape.move(0.f, -speed); break;
-                    case sf::Keyboard::Key::S:
-                        shape.move(0.f, speed); break;
-                    case sf::Keyboard::Key::A:
-                        shape.move(-speed, 0.f); break;
-                    case sf::Keyboard::Key::D:
-                        shape.move(speed, 0.f); break;
-                    default: break;
-                }
-            }
+
+        for (unsigned cycle{ 0 };
+            cycle < cycles_per_frame;
+            ++cycle)
+        {
+            canvas.process_events(chip8);
+            chip8.emulate_cycle();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+
+        if (chip8.should_draw()) {
+            canvas.update(chip8.framebuffer());
+            canvas.redraw();
+            chip8.reset_draw_flag();
+        }
+
+        std::this_thread::sleep_until(frame_clock);
 
     }
 
